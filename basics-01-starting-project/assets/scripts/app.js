d411ca06f757c2c5d944f414d618e1c25eb1936d
haran seok <gkfks222@hanmail.net>
2024-01-10 17:08:58 +0900

    udemy - 강의 내용 정리

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -2,15 +2,47 @@ const defaultResult = 0;
 
 let currentResult = defaultResult;
 
-function add(num1, num2) {
-  const result = num1 + num2;
-  return result;
+function getUserNumberInput() {
+  return parseInt(userInput.value);
 }
 
-currentResult = add(1, 2);
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDesctiption = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDesctiption);
+}
 
-currentResult = ((defaultResult + 10) * 3) / 2 - 1;
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  // Number로 타입 자체를 변경할 수 도 있고 - 내가 스스로 타입 변환 해본 것
+  // 1. currentResult = currentResult + Number(userInput.value);
+  // 소수점 이하 숫자가 없는 수로 구문 분석을 하는 자바스크립트 내장 함수인 parseInt()를 사용할 수 있다. - 강의에서 알려준 방법
+  currentResult = currentResult + enteredNumber;
+  createAndWriteOutput("+", initialResult, enteredNumber);
+}
 
-let calculationDescription = `( ${defaultResult} + 10 ) * 3 / 2 - 1`;
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - enteredNumber;
+  createAndWriteOutput("-", initialResult, enteredNumber);
+}
 
-outputResult(currentResult, calculationDescription);
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNumber;
+  createAndWriteOutput("*", initialResult, enteredNumber);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNumber;
+  createAndWriteOutput("/", initialResult, enteredNumber);
+}
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
