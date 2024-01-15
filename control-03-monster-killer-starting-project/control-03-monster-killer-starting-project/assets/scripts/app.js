20096f13fb2d21a9b212734fd605a6044d79b440
haran seok <gkfks222@hanmail.net>
2024-01-15 19:01:22 +0900

    udemy_ep.92

--- control-03-monster-killer-starting-project/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,13 +1,22 @@
 // 하드코딩한 전역 상수의 경우 대문자 및 snake_case로 작성하는 것이 일반적이다. 필수는 아니고 선택사항이다.
-
+// 대문자로 작성한 이유는 이 코드는 모두 정적 코드이고, 개발자가 첫 부분에 하드코딩한 값이라고 명확하게 하기 위함이다.
 const ATTACK_VALUE = 10;
 const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
+const MODE_ATTACT = "ATTACK"; // MODE_ATTACT = 0
+const MODE_STRONG_ATTACT = "STRONG_ATTACT"; // MODE_STRONG_ATTACT = 1
+const LOG_EVENT_PLAYER_ATTACK = "PLAYER_ATTACT";
+const LOG_EVENT_PLAYER_STRONG_ATTACK = "PLAYER_STRONG_ATTACK";
+const LOG_EVENT_MONSTER_ATTACK = "MONSTER_ATTACT";
+const LOG_EVENT_PLAYER_HEAL = "PLAYER_HEAL";
+const LOG_EVENT_GAME_OVER = "GAME_OVER";
+
 const enterdValue = prompt("Maxinum life for you and the monster.", "100");
 
 let chosenMaxLife = parseInt(enterdValue);
+let battleLog = [];
 
 //OR 연산자의 경우 첫번째 조건이 참이면 멈추고 조건문 안의 내용을 실행한다.
 
@@ -20,6 +29,27 @@ let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
+
+function writeToLog(ev, val, monsterHealth, playerHealth) {
+  let logEntry;
+
+  if (ev === LOG_EVENT_PLAYER_ATTACK) {
+    logEntry = {
+      event: ev,
+      value: val,
+      finalMonsterHealth: monsterHealth,
+      finalPlayerHealth: playerHealth,
+    };
+    battleLog.push(logEntry);
+  } else if (ev === LOG_EVENT_PLAYER_STRONG_ATTACK) {
+    logEntry = {
+      event: ev,
+      value: val,
+      finalMonsterHealth: monsterHealth,
+      finalPlayerHealth: playerHealth,
+    };
+  }
+}
 
 function reset() {
   currentMonsterHealth = chosenMaxLife;
@@ -57,9 +87,9 @@ function endRound() {
 
 function attackMonster(mode) {
   let maxDamage;
-  if (mode === "ATTACK") {
+  if (mode === MODE_ATTACT) {
     maxDamage = ATTACK_VALUE;
-  } else if (mode === "STRONG_ATTACK") {
+  } else if (mode === MODE_STRONG_ATTACT) {
     maxDamage = STRONG_ATTACK_VALUE;
   }
   const damage = dealMonsterDamage(maxDamage);
@@ -68,11 +98,11 @@ function attackMonster(mode) {
 }
 
 function attackHandler() {
-  attackMonster("ATTACK");
+  attackMonster(MODE_ATTACT);
 }
 
 function strongAttackHandler() {
-  attackMonster("STRONG_ATTACK");
+  attackMonster(MODE_STRONG_ATTACT);
 }
 
 function healPlayerHandler() {
