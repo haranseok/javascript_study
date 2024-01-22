528e5324cb508dc0983a2fe3858f34404e8d50a5
Haran <gkfks222@hanmail.net>
2024-01-22 23:19:44 +0900

    udemy_섹션7 DOM 으로 작업하기 완강

--- dom-06-demo-project-setup/dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/dom-06-demo-project-setup/assets/scripts/app.js
@@ -2,42 +2,108 @@
 // 버튼에 id를 추가해서 getElementById로 값을 가지고 오는 방식
 // const startAddMovieButton = document.querySelector("header").lastElementChild;
 // 이 방법도 선택할 수 잇지만 header 안에 마지막 Element가 아닐수도 있다.
-const addMovieModal = document.getElementById("add-modal");
-const startAddMovieButton = document.querySelector("header button");
-const backdrop = document.getElementById("backdrop");
-const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
-const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
-const userInputs = addMovieModal.querySelectorAll("input");
-const entryTextSection = document.getElementById("entry-text");
-const movieList = document.getElementById("movie-list");
+const addMovieModal = document.getElementById('add-modal');
+const startAddMovieButton = document.querySelector('header button');
+const backdrop = document.getElementById('backdrop');
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const confirmAddMovieButton = addMovieModal.querySelector('.btn--success');
+const userInputs = addMovieModal.querySelectorAll('input');
+const entryTextSection = document.getElementById('entry-text');
+const movieList = document.getElementById('movie-list');
+const deleteMovieModal = document.getElementById('delete-modal');
 
 const movies = [];
 
+const toggleBackdrop = () => {
+  backdrop.classList.toggle('visible');
+};
+
 const updateUI = () => {
   if (movies.length === 0) {
-    entryTextSection.style.display = "block";
+    entryTextSection.style.display = 'block';
   } else {
-    entryTextSection.style.display = "none";
+    entryTextSection.style.display = 'none';
   }
 };
 
-const toggleBackdrop = () => {
-  backdrop.classList.toggle("visible");
+const closeMovieDeletionModal = () => {
+  toggleBackdrop();
+  deleteMovieModal.classList.remove('visible');
 };
 
-const toggleMovieModal = () => {
-  addMovieModal.classList.toggle("visible");
+const deleteMovie = movieId => {
+  let movieIndex = 0;
+
+  for (const movie of movies) {
+    if (movie.id === movieId) {
+      break;
+    }
+    movieIndex++;
+  }
+
+  movies.splice(movieIndex, 1);
+  const listRoot = document.getElementById('movie-list');
+  listRoot.children[movieIndex].remove();
+  closeMovieDeletionModal();
+  updateUI();
+};
+
+const deleteMovieHandler = movieId => {
+  deleteMovieModal.classList.add('visible');
+  toggleBackdrop();
+  const cancelDeletionButton = deleteMovieModal.querySelector('.btn--passive');
+  let confirmDeletionButton = deleteMovieModal.querySelector('.btn--danger');
+  /** 버튼을 여러번 클릭하게 되면 어떤 버튼을 클릭했는 지 판단할 수 없어 error가 발생한다.
+   * 그래서 이벤트리스너를 추가하기 전에 DeletionButton을 바꿔줘서 함수가 재 실행할 때마다 새로운 DeletionButton을 갖는 것이다.
+   * 가비지 컬렉터의 활용...?
+   */
+
+  confirmDeletionButton.replaceWith(confirmDeletionButton.cloneNode(true));
+  confirmDeletionButton = deleteMovieModal.querySelector('.btn--danger');
+  cancelDeletionButton.removeEventListener('click', closeMovieDeletionModal);
+  cancelDeletionButton.addEventListener('click', closeMovieDeletionModal);
+  confirmDeletionButton.addEventListener(
+    'click',
+    deleteMovie.bind(null, movieId)
+  );
+};
+
+const renderNewMovieElement = (id, title, imgUrl, rating) => {
+  const newMovieElement = document.createElement('li');
+  newMovieElement.className = 'movie-element';
+  newMovieElement.innerHTML = `
+    <div class='movie-element__img'>
+      <img src="${imgUrl}" alt="${title}">
+    </div>
+    <div class=movie-element__info>
+        <h2>${title}</h2>
+        <p>${rating}/5 stars</p>
+    </div>
+  `;
+
+  newMovieElement.addEventListener('click', deleteMovieHandler.bind(null, id));
+  const listRoot = document.getElementById('movie-list');
+  listRoot.append(newMovieElement);
+};
+
+const closeMovieModal = () => {
+  addMovieModal.classList.remove('visible');
+};
+
+const showMovieModal = () => {
+  addMovieModal.classList.add('visible');
   toggleBackdrop();
 };
 
 const clearMovieInput = () => {
   for (const userInput of userInputs) {
-    userInput.value = "";
+    userInput.value = '';
   }
 };
 
 const cancelAddMovieHandler = () => {
-  toggleMovieModal();
+  closeMovieModal();
+  toggleBackdrop();
   clearMovieInput();
 };
 
@@ -45,37 +111,46 @@ const addMovieHandler = () => {
   const titleValue = userInputs[0].value;
   const imgUrlValue = userInputs[1].value;
   const ratingValue = userInputs[2].value;
-
   // trim() 불필요한 공백을 없애준다. 단 단어와 단어사이에  있는 공백은 제외
   if (
-    titleValue.trim() === "" ||
-    imgUrlValue.trim() === "" ||
-    ratingValue.trim() === "" ||
+    titleValue.trim() === '' ||
+    imgUrlValue.trim() === '' ||
+    ratingValue.trim() === '' ||
     +ratingValue < 1 ||
     +ratingValue > 5
   ) {
-    alert("Please enter valid values (rating between 1 and 5).");
+    alert('Please enter valid values (rating between 1 and 5).');
     return;
   }
 
   const newMovie = {
+    id: Math.random().toString(),
     title: titleValue,
     image: imgUrlValue,
     rating: ratingValue,
   };
 
   movies.push(newMovie);
-  toggleMovieModal();
+  closeMovieModal();
+  toggleBackdrop();
   clearMovieInput();
+  renderNewMovieElement(
+    newMovie.id,
+    newMovie.title,
+    newMovie.image,
+    newMovie.rating
+  );
   updateUI();
 };
 
 const backdropClickHandler = () => {
-  toggleMovieModal();
+  closeMovieModal();
+  closeMovieDeletionModal();
+  clearMovieInput();
 };
 
-startAddMovieButton.addEventListener("click", toggleMovieModal);
-backdrop.addEventListener("click", backdropClickHandler);
+startAddMovieButton.addEventListener('click', showMovieModal);
+backdrop.addEventListener('click', backdropClickHandler);
 // backdrop.addEventListener("click", toggleMovieModal); 이렇게 사용하게되면 정상적으로 동작은 하고 있지만 backdrop을 별도의 함수로 만들어서 관리하는 것이 함수의 재사용성을 높을 수 있다.
-cancelAddMovieButton.addEventListener("click", cancelAddMovieHandler);
-confirmAddMovieButton.addEventListener("click", addMovieHandler);
+cancelAddMovieButton.addEventListener('click', cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener('click', addMovieHandler);
