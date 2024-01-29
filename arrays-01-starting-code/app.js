f3d9ba9b328ba1179262c0dc97ced6e8d5d9449c
Haran <gkfks222@hanmail.net>
2024-01-29 23:21:33 +0900

    udemy filter, reduce등 배열 메서드 정리

--- arrays-01-starting-code/app.js
+++ arrays-01-starting-code/app.js
@@ -82,9 +82,9 @@ console.log(testResults.includes(10.99)); // true
 // lastIndexOf() - 오른쪽에서부터 인덱스를 찾을 때 사용
 
 //indexOf()와 lastIndexOf()의 중요한 점은 원시 값에 관해서는 실행이 되지만 참조 값에 관해서는 실행이 안된다.
-const personData = [{ name: "Max" }, { name: "Manuel" }];
+const personData = [{ name: 'Max' }, { name: 'Manuel' }];
 
-console.log(personData.indexOf({ name: "Manuel" })); // -1 === false
+console.log(personData.indexOf({ name: 'Manuel' })); // -1 === false
 
 //객체는 참조 값이므로 새 객체를 indexOf()로 보내면 indexOf의 모든 값과 personData의 모든 값을 비교하지만 두 객체는 비슷해보여도 절대 비슷하지 않다. indexOf는 객체가 있으면 실행되지 않는다.
 
@@ -94,12 +94,12 @@ console.log(personData.indexOf({ name: "Manuel" })); // -1 === false
 // find()는 객체가 있는 배열에만 국한되지 않고, 모든 배열에서 사용가능하다.
 // find()는 찾으려고 하는 요소에 관해 true가 그렇지 않으면 fasle를 반환한다.
 const manuel = personData.find((person, idx, persons) => {
-  return person.name === "Max";
+  return person.name === 'Max';
 });
 
 console.log(manuel); // { name: 'Max' }
 
-manuel.name = "Anna";
+manuel.name = 'Anna';
 
 // manuel.name = Anna로 변경 , personData에서도 Anna로 값이 변경된 것을 확인할 수 있다.
 // 동일한 객체의 동일한 참조 값으로 작업하고 있기 때문에 find()는 복사를 생성하지 않는 다.
@@ -107,7 +107,7 @@ console.log(manuel, personData); // { name: 'Max' }
 
 // findIndex() 찾고자 하는 값의 index를 반환한다.
 const maxIndex = personData.findIndex((person, idx, persons) => {
-  return person.name === "Max";
+  return person.name === 'Max';
 });
 
 console.log(maxIndex); // 0
@@ -140,3 +140,69 @@ const taxAdustedPrices = prices.map((price, idx, prices) => {
 });
 
 console.log(taxAdustedPrices);
+
+// sort() 메서드
+/**
+ * 배열을 정렬할 수 있다.
+ * 문자열 논리로 정렬한다. ( 문자열은 기본적으로 첫 번째 문자열만 비교 ex. 10 > 3 이 아닌 '10' > '3' 이어서 1과 3을 비교한다.)
+ * **/
+
+// const sortedPrices = prices.sort(); // 이 형식으로 하게 되면 [10.99, 3.99, 5.99, 6.59] 이렇게 출력된다.
+
+const sortedPrices = prices.sort((a, b) => {
+  if (a > b) {
+    // 양수인 1을 반환
+    return 1;
+  } else if (a === b) {
+    // 동일하면 0
+    return 0;
+  } else {
+    return -1; // b가 더 크면 -1
+  }
+});
+
+console.log(sortedPrices.reverse());
+
+// reverse() // 배열을 반전한다.
+
+// filter()
+
+// const filteredArray = prices.filter((price, index, prices) => {
+//   return price > 6; // true or false
+// });
+
+// console.log(filteredArray);
+
+// 화살표 함수의 장점
+/**
+ * 코드를 정말 짧게 작성할 수 있다.
+ * 인수가 1개인 경우엔 괄호와 중괄호를 삭제할 수 있다.
+ * return 문을 삭제하고 세밐콜론까지 삭제할 수 있다.
+ * **/
+
+const filteredArray = prices.filter(price => price > 6);
+
+// reduce() 메서드 ( 중요 )
+
+/**
+ * 배열 안에 있는 값을 더할 때 사용할 수 있다.
+ * 아래와 같이 forEach를 사용할 수 있지만, reduce를 사용하면 더 간단하게 코드를 작성할 수 있다.
+ *
+ * reduce 메서드는 배열에서 사용 가능한 빌드인 메서드로 함수를 취한다. 여러 개의 인수를 취한다.
+ * 배열을 더 단순한 값으로 줄이는 것으로 주로 배열을 단일 숫자나, 단일 문자열로 줄인다.
+ * **/
+
+let sum = 0;
+
+prices.forEach(price => {
+  sum += price;
+});
+
+console.log(sum); //27.560000000000002
+
+// curIndex, prices 는 잘 사용하지 않는 인수
+// reduce의 두 번째 인수는 시작하려는 초기 값이다. prevValue 가 초기 값으로 설정하지 않으면 undefind가 된다. 현재 코드에서는 0으로 설정되어있다.
+
+const sum2 = prices.reduce((prevValue, curValue, curIndex, prices) => {
+  return prevValue + curValue;
+}, 0);
