6133b6c734982aab7bd702c7eed074e91f9df003
Haran <gkfks222@hanmail.net>
2024-02-05 23:33:19 +0900

    udemy_221 검색 필터

--- objects-01-starting-project/objects-01-starting-project/assets/scripts/objects.js
+++ objects-01-starting-project/objects-01-starting-project/assets/scripts/objects.js
@@ -3,7 +3,7 @@ const searchBtn = document.getElementById('search-btn');
 
 const movies = [];
 
-const renderMovies = () => {
+const renderMovies = (filter = '') => {
   const movieList = document.getElementById('movie-list');
   if (movies.length === 0) {
     movieList.classList.remove('visible');
@@ -13,7 +13,11 @@ const renderMovies = () => {
 
   movieList.innerHTML = '';
 
-  movies.forEach(movie => {
+  const filteredMovies = !filter
+    ? movies
+    : movies.filter(movie => movie.info.title.includes(filter));
+
+  filteredMovies.forEach(movie => {
     const movieEl = document.createElement('li');
     let text = movie.info.title + ' - ';
     for (const key in movie.info) {
@@ -51,4 +55,10 @@ const addMovueHandler = () => {
   renderMovies();
 };
 
+const searchMovieHadler = () => {
+  const filterTerm = document.getElementById('filter-title').value;
+  renderMovies(filterTerm);
+};
+
 addMovieBtn.addEventListener('click', addMovueHandler);
+searchBtn.addEventListener('click', searchMovieHadler);
