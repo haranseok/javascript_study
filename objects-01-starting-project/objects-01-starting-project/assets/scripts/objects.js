d19cd84583fa39b3be38b1bb07cb417f71199357
haran seok <gkfks222@hanmail.net>
2024-02-13 10:01:22 +0900

    udemy_226 객체 구조 분해

--- objects-01-starting-project/objects-01-starting-project/assets/scripts/objects.js
+++ objects-01-starting-project/objects-01-starting-project/assets/scripts/objects.js
@@ -1,28 +1,30 @@
-const addMovieBtn = document.getElementById('add-movie-btn');
-const searchBtn = document.getElementById('search-btn');
+const addMovieBtn = document.getElementById("add-movie-btn");
+const searchBtn = document.getElementById("search-btn");
 
 const movies = [];
 
-const renderMovies = (filter = '') => {
-  const movieList = document.getElementById('movie-list');
+const renderMovies = (filter = "") => {
+  const movieList = document.getElementById("movie-list");
   if (movies.length === 0) {
-    movieList.classList.remove('visible');
+    movieList.classList.remove("visible");
   } else {
-    movieList.classList.add('visible');
+    movieList.classList.add("visible");
   }
 
-  movieList.innerHTML = '';
+  movieList.innerHTML = "";
 
   const filteredMovies = !filter
     ? movies
-    : movies.filter(movie => movie.info.title.includes(filter));
+    : movies.filter((movie) => movie.info.title.includes(filter));
 
-  filteredMovies.forEach(movie => {
-    const movieEl = document.createElement('li');
-    let text = movie.info.title + ' - ';
-    for (const key in movie.info) {
-      if (key !== 'title') {
-        text = text + `${key}: ${movie.info[key]}`;
+  filteredMovies.forEach((movie) => {
+    const movieEl = document.createElement("li");
+    const { info } = movie; // 우측에 존재하는 key 이름을 좌측에 입력, 객체에서 프로퍼티를 빼낼때에는 순서는 중요하지 않고, key 이름이 중요하다. - 객체 구조 분해
+    const { title: movieTitle } = info; // 객체 구조 분해는 key 이름이 중요하지만, 다른 곳에서 사용하고 있는 경우 충돌을 막기 위해 다른 이름으로 지정할 수 있다. 기존 key: 새로운 key
+    let text = movieTitle + " - ";
+    for (const key in info) {
+      if (key !== "title") {
+        text = text + `${key}: ${info[key]}`;
       }
     }
     movieEl.textContent = text;
@@ -31,14 +33,14 @@ const renderMovies = (filter = '') => {
 };
 
 const addMovueHandler = () => {
-  const title = document.getElementById('title').value;
-  const extraName = document.getElementById('extra-name').value;
-  const extraValue = document.getElementById('extra-value').value;
+  const title = document.getElementById("title").value;
+  const extraName = document.getElementById("extra-name").value;
+  const extraValue = document.getElementById("extra-value").value;
 
   if (
-    title.trim() === '' ||
-    extraName.trim() === '' ||
-    extraValue.trim() === ''
+    title.trim() === "" ||
+    extraName.trim() === "" ||
+    extraValue.trim() === ""
   ) {
     return;
   }
@@ -56,9 +58,9 @@ const addMovueHandler = () => {
 };
 
 const searchMovieHadler = () => {
-  const filterTerm = document.getElementById('filter-title').value;
+  const filterTerm = document.getElementById("filter-title").value;
   renderMovies(filterTerm);
 };
 
-addMovieBtn.addEventListener('click', addMovueHandler);
-searchBtn.addEventListener('click', searchMovieHadler);
+addMovieBtn.addEventListener("click", addMovueHandler);
+searchBtn.addEventListener("click", searchMovieHadler);
