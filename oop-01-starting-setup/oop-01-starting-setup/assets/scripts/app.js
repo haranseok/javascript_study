fcb37ae8ac232ffc287e83573d47ea260aa5695c
Haran <gkfks222@hanmail.net>
2024-02-19 23:19:20 +0900

    udemy_253_클래스 상속 extends

--- oop-01-starting-setup/oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/oop-01-starting-setup/assets/scripts/app.js
@@ -34,7 +34,37 @@ class Product {
   } // 예약어
 }
 
-class ShoppingCart {
+class ElementAttribute {
+  constructor(attrName, attrValue) {
+    this.name = attrName;
+    this.value = attrValue;
+  }
+}
+
+class Component {
+  constructor(renderHookId) {
+    this.hookId = renderHookId;
+  }
+
+  // 3개의 인수를 가지는 메서드 - 개발자가 만드는 메서드임
+  createRootElement(tag, cssClasses, attributes) {
+    const rootElement = document.createElement(tag);
+    // css 상수가 설정되었는 지 확인.
+    if (cssClasses) {
+      rootElement.className = cssClasses;
+    }
+    if (attributes && attributes.lenghth > 0) {
+      for (const attr of attributes) {
+        rootElement.setAttribute(attr.name, attr.value);
+      }
+    }
+
+    document.getElementById(this.hookId).append(rootElement);
+    return rootElement;
+  }
+}
+
+class ShoppingCart extends Component {
   items = [];
 
   set cartItems(value) {
@@ -51,6 +81,11 @@ class ShoppingCart {
     return sum;
   }
 
+  // 부모 생성자를 호출하는 것으로 확장하는 클래스인 부모 클래스의 생성자를 호출
+  constructor(renderHookId) {
+    super(renderHookId);
+  }
+
   addProduct(product) {
     const updatedItems = [...this.items];
     updatedItems.push(product);
@@ -59,14 +94,12 @@ class ShoppingCart {
 
   render() {
     // 총 계, 주문 버튼 등 장바구니 내부 로직
-    const cartEl = document.createElement("section");
+    const cartEl = this.createRootElement('section', 'cart');
     cartEl.innerHTML = `
     <h2>Total : \$${0}</h2>
     <button>Order Now!</button>
     `;
-    cartEl.className = "cart";
-    this.totalOutPut = cartEl.querySelector("h2");
-    return cartEl;
+    this.totalOutPut = cartEl.querySelector('h2');
   }
 }
 
@@ -81,8 +114,8 @@ class ProductItem {
 
   // 메서드 명은 꼭 render가 아니라 원하는 것으로 지을 수 있다.
   render() {
-    const prodEl = document.createElement("li");
-    prodEl.className = "product-item";
+    const prodEl = document.createElement('li');
+    prodEl.className = 'product-item';
     prodEl.innerHTML = `
       <div>
         <img src="${this.product.imageUrl}" alt="${this.product.title}">
@@ -97,8 +130,8 @@ class ProductItem {
     // add 버튼이 동작하도록 구현
     // 단일 상품을 만드는 단일 클래스이기 때문에 여러 개의 버튼이 생성되어도 문제가 되지 않는다.
 
-    const addCartButton = prodEl.querySelector("button");
-    addCartButton.addEventListener("click", this.addToCart.bind(this)); // bind(this)를 하지않는 경우 이벤트 소스에 this가 바인딩되어 undefined가 나오게 된다.
+    const addCartButton = prodEl.querySelector('button');
+    addCartButton.addEventListener('click', this.addToCart.bind(this)); // bind(this)를 하지않는 경우 이벤트 소스에 this가 바인딩되어 undefined가 나오게 된다.
     return prodEl;
   }
 }
@@ -106,15 +139,15 @@ class ProductItem {
 class ProductList {
   poroducts = [
     new Product(
-      "A Pillow",
-      "https://fastly.picsum.photos/id/240/200/300.jpg?blur=5&hmac=UDFJH4m-QKTtSXWzQ0e4-5wAlhTut6kOBmgTqRMFgt8",
-      "A soft pillow!",
+      'A Pillow',
+      'https://fastly.picsum.photos/id/240/200/300.jpg?blur=5&hmac=UDFJH4m-QKTtSXWzQ0e4-5wAlhTut6kOBmgTqRMFgt8',
+      'A soft pillow!',
       19.99
     ),
     new Product(
-      "A Carpet",
-      "https://fastly.picsum.photos/id/240/200/300.jpg?blur=5&hmac=UDFJH4m-QKTtSXWzQ0e4-5wAlhTut6kOBmgTqRMFgt8",
-      "A carpet which you migth like - or not.",
+      'A Carpet',
+      'https://fastly.picsum.photos/id/240/200/300.jpg?blur=5&hmac=UDFJH4m-QKTtSXWzQ0e4-5wAlhTut6kOBmgTqRMFgt8',
+      'A carpet which you migth like - or not.',
       89.99
     ),
   ];
@@ -122,8 +155,8 @@ class ProductList {
   constructor() {}
 
   render() {
-    const prodList = document.createElement("ul");
-    prodList.className = "product-list";
+    const prodList = document.createElement('ul');
+    prodList.className = 'product-list';
     // 논리 추가
     for (const prod of this.poroducts) {
       const productItem = new ProductItem(prod);
@@ -136,14 +169,12 @@ class ProductList {
 
 class Shop {
   render() {
-    const renderHook = document.getElementById("app");
+    const renderHook = document.getElementById('app');
 
-    this.cart = new ShoppingCart();
-
-    const cartEl = this.cart.render();
+    this.cart = new ShoppingCart('app');
+    this.cart.render();
     const productList = new ProductList();
     const prodListEl = productList.render();
-    renderHook.append(cartEl);
     renderHook.append(prodListEl);
   }
 }
