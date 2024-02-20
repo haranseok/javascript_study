978913ddd3fd2631581a242d0a66273da80f232f
haran seok <gkfks222@hanmail.net>
2024-02-20 17:45:50 +0900

    udemy_~257

--- oop-01-starting-setup/oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/oop-01-starting-setup/assets/scripts/app.js
@@ -1,25 +1,3 @@
-// const poroducts = [
-//   {
-//     title: "A Pillow",
-//     imageUrl:
-//       "https://fastly.picsum.photos/id/240/200/300.jpg?blur=5&hmac=UDFJH4m-QKTtSXWzQ0e4-5wAlhTut6kOBmgTqRMFgt8",
-//     price: 19.99,
-//     description: "A soft pillow!",
-//   },
-//   {
-//     title: "A Carpet",
-//     imageUrl:
-//       "https://fastly.picsum.photos/id/240/200/300.jpg?blur=5&hmac=UDFJH4m-QKTtSXWzQ0e4-5wAlhTut6kOBmgTqRMFgt8",
-//     price: 89.99,
-//     description: "A carpet which you migth like - or not.",
-//   },
-// ];
-
-// 모든 논리가 productList 객체로 담김
-// 객체 리터널 표기법은 객체를 바로 만들거나 데이터를 그룹으로 묶을 때에는 유용하다. 하지만 객체 리터널 표기법을 사용하면 재사용 가능한 객체 코드를 쓰기 어렵다.
-
-// class 명의 첫 글자는 대문자로 시작하고, 모든 하위 단어의 시작을 대문자로 작성한다.
-
 class Product {
   // title = "DEFAULT"; // 기본 값을 설정, class 정의 안에는 값을 등호로 작성한다.
   // imageUrl; // 빈 값을 두거나 아무런 값도 작성하지 않아도 된다.
@@ -41,19 +19,24 @@ class ElementAttribute {
   }
 }
 
+// 부모 클래스
 class Component {
-  constructor(renderHookId) {
+  constructor(renderHookId, shouldRender = true) {
     this.hookId = renderHookId;
+    if (shouldRender) {
+      this.render();
+    }
   }
 
-  // 3개의 인수를 가지는 메서드 - 개발자가 만드는 메서드임
+  render() {}
+  // 빈 메스드 불필요하긴 하나 추가하는 이유는 constructor에 사용된 this.render()가 왜 호출되는 지 바로 알 수 있게 하기 위함이다.
+
   createRootElement(tag, cssClasses, attributes) {
     const rootElement = document.createElement(tag);
-    // css 상수가 설정되었는 지 확인.
     if (cssClasses) {
       rootElement.className = cssClasses;
     }
-    if (attributes && attributes.lenghth > 0) {
+    if (attributes && attributes.length > 0) {
       for (const attr of attributes) {
         rootElement.setAttribute(attr.name, attr.value);
       }
@@ -69,137 +52,154 @@ class ShoppingCart extends Component {
 
   set cartItems(value) {
     this.items = value;
-    this.totalOutPut.innerHTML = `<h2>Total : \$${this.totlaAmount.toFixed(
+    this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(
       2
     )}</h2>`;
   }
 
-  get totlaAmount() {
-    const sum = this.items.reduce((prevValue, curItem) => {
-      return prevValue + curItem.price;
-    }, 0);
+  get totalAmount() {
+    const sum = this.items.reduce(
+      (prevValue, curItem) => prevValue + curItem.price,
+      0
+    );
     return sum;
   }
 
-  // 부모 생성자를 호출하는 것으로 확장하는 클래스인 부모 클래스의 생성자를 호출
   constructor(renderHookId) {
-    super(renderHookId);
+    super(renderHookId, false);
+    this.orderProducts = () => {
+      console.log("Order");
+      console.log(this.items);
+    };
+    this.render();
   }
 
   addProduct(product) {
     const updatedItems = [...this.items];
     updatedItems.push(product);
-    this.cartItems = updatedItems; // setter를 트리거하고 cartItems의 value params로 전달하여 코드를 업데이트하게 된다.
+    this.cartItems = updatedItems;
   }
 
   render() {
-    // 총 계, 주문 버튼 등 장바구니 내부 로직
-    const cartEl = this.createRootElement('section', 'cart');
+    const cartEl = this.createRootElement("section", "cart");
     cartEl.innerHTML = `
-    <h2>Total : \$${0}</h2>
-    <button>Order Now!</button>
-    `;
-    this.totalOutPut = cartEl.querySelector('h2');
+        <h2>Total: \$${0}</h2>
+        <button>Order Now!</button>
+      `;
+    const orderButton = cartEl.querySelector("button");
+    // orderButton.addEventListener("click", () => this.orderProducts());
+    orderButton.addEventListener("click", this.orderProducts);
+
+    this.totalOutput = cartEl.querySelector("h2");
   }
 }
 
-class ProductItem {
-  constructor(product) {
+class ProductItem extends Component {
+  constructor(product, renderHookId) {
+    super(renderHookId, false); // false : shouldRender
     this.product = product;
+    this.render();
   }
 
   addToCart() {
-    App.addProductToCart(this.product); // ProductItem에 저장된 product를 의미
+    App.addProductToCart(this.product);
   }
 
-  // 메서드 명은 꼭 render가 아니라 원하는 것으로 지을 수 있다.
   render() {
-    const prodEl = document.createElement('li');
-    prodEl.className = 'product-item';
+    const prodEl = this.createRootElement("li", "product-item");
     prodEl.innerHTML = `
-      <div>
-        <img src="${this.product.imageUrl}" alt="${this.product.title}">
-        <div class="product-item__content">
-        <h2>${this.product.title}</h2>
-        <h3>\$${this.product.price}</h3>
-        <p>${this.product.description}</p>
-        <button>Add to Cart</button>
-        </div>
-      </div>
-      `;
-    // add 버튼이 동작하도록 구현
-    // 단일 상품을 만드는 단일 클래스이기 때문에 여러 개의 버튼이 생성되어도 문제가 되지 않는다.
-
-    const addCartButton = prodEl.querySelector('button');
-    addCartButton.addEventListener('click', this.addToCart.bind(this)); // bind(this)를 하지않는 경우 이벤트 소스에 this가 바인딩되어 undefined가 나오게 된다.
-    return prodEl;
+          <div>
+            <img src="${this.product.imageUrl}" alt="${this.product.title}" >
+            <div class="product-item__content">
+              <h2>${this.product.title}</h2>
+              <h3>\$${this.product.price}</h3>
+              <p>${this.product.description}</p>
+              <button>Add to Cart</button>
+            </div>
+          </div>
+        `;
+    const addCartButton = prodEl.querySelector("button");
+    addCartButton.addEventListener("click", this.addToCart.bind(this));
   }
 }
 
-class ProductList {
-  poroducts = [
-    new Product(
-      'A Pillow',
-      'https://fastly.picsum.photos/id/240/200/300.jpg?blur=5&hmac=UDFJH4m-QKTtSXWzQ0e4-5wAlhTut6kOBmgTqRMFgt8',
-      'A soft pillow!',
-      19.99
-    ),
-    new Product(
-      'A Carpet',
-      'https://fastly.picsum.photos/id/240/200/300.jpg?blur=5&hmac=UDFJH4m-QKTtSXWzQ0e4-5wAlhTut6kOBmgTqRMFgt8',
-      'A carpet which you migth like - or not.',
-      89.99
-    ),
-  ];
+class ProductList extends Component {
+  // super가 실생되기 전에 어떤 = 어떤을 입력하면 안된다.
+  poroducts = [];
 
-  constructor() {}
+  constructor(renderHookId) {
+    super(renderHookId);
+    this.fetchProducts();
+  }
 
-  render() {
-    const prodList = document.createElement('ul');
-    prodList.className = 'product-list';
-    // 논리 추가
+  fetchProducts() {
+    this.poroducts = [
+      new Product(
+        "A Pillow",
+        "https://fastly.picsum.photos/id/240/200/300.jpg?blur=5&hmac=UDFJH4m-QKTtSXWzQ0e4-5wAlhTut6kOBmgTqRMFgt8",
+        "A soft pillow!",
+        19.99
+      ),
+      new Product(
+        "A Carpet",
+        "https://fastly.picsum.photos/id/240/200/300.jpg?blur=5&hmac=UDFJH4m-QKTtSXWzQ0e4-5wAlhTut6kOBmgTqRMFgt8",
+        "A carpet which you migth like - or not.",
+        89.99
+      ),
+    ];
+    this.renderProducts();
+  }
+
+  renderProducts() {
     for (const prod of this.poroducts) {
-      const productItem = new ProductItem(prod);
-      const prodEl = productItem.render();
-      prodList.append(prodEl);
+      new ProductItem(prod, "prod-list");
     }
-    return prodList;
   }
-}
 
-class Shop {
   render() {
-    const renderHook = document.getElementById('app');
-
-    this.cart = new ShoppingCart('app');
-    this.cart.render();
-    const productList = new ProductList();
-    const prodListEl = productList.render();
-    renderHook.append(prodListEl);
+    this.createRootElement("ul", "product-list", [
+      new ElementAttribute("id", "prod-list"),
+    ]);
+    if (this.poroducts && this.poroducts.length > 0) {
+      this.renderProducts();
+    }
   }
 }
 
+class Shop extends Component {
+  constructor() {
+    super(); // super를 호출해서 render가 트리거됐는 지 확인할 수 있다.
+  }
+
+  render() {
+    // 태그 id : app
+    this.cart = new ShoppingCart("app");
+    new ProductList("app");
+  }
+}
+
+// 정적 메서드 & 프로퍼티
 class App {
-  static cart; // 정적 프로퍼티를 사용한다는 사실을 분명히할 수 있어, 가독성을 높일 수 있다. ( 필수는 아님 )
+  static cart;
 
   static init() {
-    // Shop으로 액세스할 수 있다.
     const shop = new Shop();
-    shop.render();
-    this.cart = shop.cart; // Shop 클래스에 this.cart를 보유한 객체라서 접근 가능
+    this.cart = shop.cart;
   }
 
   static addProductToCart(product) {
-    this.cart.addProduct(product); // 에러가 발생한다. addProduct를 cart에서 찾지 못 하기 때문.
+    this.cart.addProduct(product);
   }
 }
 
 App.init();
 
-// 위에 작성된 코드는 모두 독립적으로 작성된 class이다. 각 class에서 생성된 요소를 반환하고, 다른 클래스에서 사용했다.
-// addToCart() 메서드가 실행될 때, cart class로 이어지도록 작업이 되어야함. - 다시 렌더링되는 cart 클래스를 다시 업데이트하도록 되어야한다.
-
-/** 252 상속 소개
- * 위 클래스 중에서 중복적으로 사용되는 render() 메서드를 복제
- * ?? - 완전 동일한 구조로 되어있지 않은 데 어떻게 복제가 가능한가?
- */
+/** 255
+ * 오버 라이딩
+ * 모든 하위 클래스에서 렌더링된 큭수 버전을 구현하기 때문, 그래서 하위 클래스에서 부모 클래스에 정의된 모든 메서드를 오버라이딩하면 완전히 교체하게 된다.
+ *
+ * 그 다음에 부모 클래스 ( Component )에서 this.render()를 호출하면 부모 클래스의 render()를 참조하지 않고 하위 클래스를 참조한다.
+ *
+ * 그리고 this는 늘 메서드를 참조. construct는 기본적으로 생성하는 객체를 참조
+ *
+ *  */
