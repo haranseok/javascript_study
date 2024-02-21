6c7f16afe7da751f1c381426fe8f7a442033dc95
haran seok <gkfks222@hanmail.net>
2024-02-21 19:00:19 +0900

    udemy_객체 및 클래스 모듈 마무리

--- objects-01-starting-project/objects-01-starting-project/assets/scripts/objects.js
+++ objects-01-starting-project/objects-01-starting-project/assets/scripts/objects.js
@@ -1,33 +1,33 @@
-const addMovieBtn = document.getElementById('add-movie-btn');
-const searchBtn = document.getElementById('search-btn');
+const addMovieBtn = document.getElementById("add-movie-btn");
+const searchBtn = document.getElementById("search-btn");
 
 const movies = [];
 
-const renderMovies = (filter = '') => {
-  const movieList = document.getElementById('movie-list');
+const renderMovies = (filter = "") => {
+  const movieList = document.getElementById("movie-list");
   if (movies.length === 0) {
-    movieList.classList.remove('visible');
+    movieList.classList.remove("visible");
   } else {
-    movieList.classList.add('visible');
+    movieList.classList.add("visible");
   }
 
-  movieList.innerHTML = '';
+  movieList.innerHTML = "";
 
   const filteredMovies = !filter
     ? movies
-    : movies.filter(movie => movie.info.title.includes(filter));
+    : movies.filter((movie) => movie.info.title.includes(filter));
 
-  filteredMovies.forEach(movie => {
-    const movieEl = document.createElement('li');
+  filteredMovies.forEach((movie) => {
+    const movieEl = document.createElement("li");
     const { info } = movie; // 우측에 존재하는 key 이름을 좌측에 입력, 객체에서 프로퍼티를 빼낼때에는 순서는 중요하지 않고, key 이름이 중요하다. - 객체 구조 분해
     const { title: movieTitle } = info; // 객체 구조 분해는 key 이름이 중요하지만, 다른 곳에서 사용하고 있는 경우 충돌을 막기 위해 다른 이름으로 지정할 수 있다. 기존 key: 새로운 key
-    let text = movieTitle + ' - ';
+    let text = movieTitle + " - ";
 
     // let text = getFormattedTitle.call(movie) + " - "; // 두번째 또는 그 이상의 인자는 원하는 대로 추가할 수 있다.
     // let text = getFormattedTitle.apply(movie) + ' - '; // apply는 call과 매우 유사하게 함수를 바로 실행해준다. 다만 첫 번째 인자는 this이지만 그 이상의 인사를 무한으로 추가할 수 없고 하나의 인자만 추가할 수 있다. 이 인자는 배열의 형태여야 한다.
     // 차이점은 call은 쉼표로 인자를 추가할 수 있고, apply는 배열로 추가 인자를 전달하다는 것이다.
     for (const key in info) {
-      if (key !== 'title') {
+      if (key !== "title" && key !== "_title") {
         text = text + `${key}: ${info[key]}`;
       }
     }
@@ -37,14 +37,14 @@ const renderMovies = (filter = '') => {
 };
 
 const addMovueHandler = () => {
-  const title = document.getElementById('title').value;
-  const extraName = document.getElementById('extra-name').value;
-  const extraValue = document.getElementById('extra-value').value;
+  const title = document.getElementById("title").value;
+  const extraName = document.getElementById("extra-name").value;
+  const extraValue = document.getElementById("extra-value").value;
 
   if (
     // title.trim() === '' ||
-    extraName.trim() === '' ||
-    extraValue.trim() === ''
+    extraName.trim() === "" ||
+    extraValue.trim() === ""
   ) {
     return;
   }
@@ -53,8 +53,8 @@ const addMovueHandler = () => {
     info: {
       // title, // key 이름과 value의 이름이 동일한 경우 title: title 이렇게 쓰지 않고 한 번만 쓸 수 있다.
       set title(val) {
-        if (val.trim() === '' || key !== '_title') {
-          this._title = 'DEFAULT';
+        if (val.trim() === "" || key !== "_title") {
+          this._title = "DEFAULT";
           return;
         }
         this._title = val; // newMovie를 참조하는 this로 프로퍼티를 타켓팅할 수 있음.
@@ -82,9 +82,9 @@ const addMovueHandler = () => {
 };
 
 const searchMovieHadler = () => {
-  const filterTerm = document.getElementById('filter-title').value;
+  const filterTerm = document.getElementById("filter-title").value;
   renderMovies(filterTerm);
 };
 
-addMovieBtn.addEventListener('click', addMovueHandler);
-searchBtn.addEventListener('click', searchMovieHadler);
+addMovieBtn.addEventListener("click", addMovueHandler);
+searchBtn.addEventListener("click", searchMovieHadler);
