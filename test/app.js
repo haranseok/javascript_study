b2ea45d24bd4cf42a0d150baa726d8814842a37a
haran seok <gkfks222@hanmail.net>
2024-06-28 09:56:54 +0900

    node.js 파일 생성만 진행된 상태

--- test/app.js
+++ test/app.js
@@ -1,20 +1,22 @@
 // next 반복자
 
+const { privateName } = require("@babel/types");
+
 const company = {
   curEmployee: 0,
   employees: ["Max", "Manu", "Anna"],
-  next() {
-    // 꼭 next여야함.
-    if (this.curEmployee >= this.employees.length) {
-      return { value: this.curEmployee, done: true };
-    }
-    const returnValue = {
-      value: this.employees[this.curEmployee],
-      done: false, // 반복할 값이 더 남아있는 지 여부
-    };
-    this.curEmployee++;
-    return returnValue;
-  },
+  //   next() {
+  //     // 꼭 next여야함.
+  //     if (this.curEmployee >= this.employees.length) {
+  //       return { value: this.curEmployee, done: true };
+  //     }
+  //     const returnValue = {
+  //       value: this.employees[this.curEmployee],
+  //       done: false, // 반복할 값이 더 남아있는 지 여부
+  //     };
+  //     this.curEmployee++;
+  //     return returnValue;
+  //   },
 
   // next의 내용을 사용하는 대신 제너레이터를 사용할
   [Symbol.iterator]: function* employeeGenerator() {
@@ -53,3 +55,41 @@ for (const employee of company) {
 // console.log(it.next());
 // console.log(it.next());
 // console.log(it.next());
+
+// Reflect API
+
+const course = {
+  title: "Javascript ---",
+};
+
+//setPrototypeOf 객체의 프로토타입을 설정
+
+Reflect.setPrototypeOf(course, {
+  toString() {
+    return this.title;
+  },
+});
+
+console.log(course.toString());
+
+// Proxy API
+
+const courseHandler = {
+  // 획득자
+  get(obj, propertyName) {
+    console.log(propertyName);
+    return obj[propertyName] || "NOT FOUND";
+  },
+  // 설정자
+  set(obj, propertyName, newValue) {
+    console.log("Sending data ...");
+    if (privateName === "rating") {
+      return;
+    }
+    obj[propertyName] = newValue;
+  },
+};
+
+const pCourse = new Proxy(course, courseHandler); // course 객체를 다른 객체로 랩핑, 두개의 인자 필요
+pCourse.rating = 5;
+console.log(pCourse.title, pCourse.length);
